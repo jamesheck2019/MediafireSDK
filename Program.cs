d4829f0635aa711c4e0fee0f1addc68be3337d89
jamesheck2019 <48408300+jamesheck2019@users.noreply.github.com>
2019-10-22 09:17:45 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,15 +7,6 @@ namespace CasualGamesStore
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TestForm());
-        }
+     
     }
 }
